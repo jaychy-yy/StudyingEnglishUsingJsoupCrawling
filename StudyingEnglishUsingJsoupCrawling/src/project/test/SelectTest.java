4b1dabe1a1d6fb36e4dfa612b11b55fa2c89fb80
Lee-Jin-Hyeok <jinjin0816@dsm.hs.kr>
2020-07-13 19:42:58 +0900

    SelectTest Code

---
+++ StudyingEnglishUsingJsoupCrawling/src/project/test/SelectTest.java
@@ -0,0 +1,30 @@
+package project.test;
+
+import java.io.IOException;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+public class SelectTest {
+	public static void main(String[] args) throws IOException {
+		String url = "https://docs.oracle.com/javase/8/docs/api/";
+		Document doc = Jsoup.connect(url).get();
+		
+		System.out.println("HTML : " + doc.title());
+		
+		Elements elements = doc.select("frameset");
+		System.out.println(elements.html());
+		System.out.println("\n¶ç¿ö¾²±â\n");
+		
+		Elements elements2 = doc.select("frameset").select("frame");
+		System.out.println(elements2.toString());
+		System.out.println("\n¶ç¿ö¾²±â\n");
+		
+		Elements elements3 = doc.select("frameset").select("frame");
+		for(Element e : elements3) {
+			System.out.println(e.data());
+		}
+	}
+}
--- StudyingEnglishUsingJsoupCrawling/src/project/test/Test.java
+++ StudyingEnglishUsingJsoupCrawling/src/project/test/Test.java
@@ -1,5 +1,20 @@
 package project.test;
 
-public class Test {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
 
+public class Test {
+	public static void main(String[] args) throws IOException {
+		URL url = new URL("https://docs.oracle.com/javase/8/docs/api/\"");
+		BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream()));
+		StringBuffer sourceCode = new StringBuffer();
+		
+		String sourceLine = "";
+		while((sourceLine=br.readLine()) != null) {
+			sourceCode.append(sourceLine+"\n");
+		}
+		System.out.println(sourceCode.toString());
+	}
 }
